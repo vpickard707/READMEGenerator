2dadb38a651ebba3e145b046c78e44c1f7583ada
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2020-11-25 17:52:55 -0800

    added user questions

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "location",
+    message: "Where are you from?",
+  },
 ];
 
+//1. Table of Contents (Optional)
+
+// If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+// - [Installation](#installation)
+// - [Usage](#usage)
+// - [Credits](#credits)
+// - [License](#license)
+/*## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+// The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)*/
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((input) => console.log(input));
 }
 
 // function call to initialize program
