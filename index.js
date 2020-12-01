10b0d5c26bee59688e93301b106147884e8e5f9b
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2020-12-01 15:04:44 -0800

    added questions, user input

--- index.js
+++ index.js
@@ -5,16 +5,38 @@ const inquirer = require("inquirer");
 const questions = [
   {
     type: "input",
-    name: "name",
-    message: "What is your name?",
+    name: "installation",
+    message:
+      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
   },
   {
     type: "input",
-    name: "location",
-    message: "Where are you from?",
+    name: "credits",
+    message:
+      "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
+  },
+
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "Provide instructions and examples for use. Include screenshots as needed.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message:
+      "The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. The following OSI-approved licenses are popular, widely used, or have strong communities. Apache License 2.0, BSD 3-Clause 'New' or 'Revised' license, BSD 2-Clause 'Simplified' or 'FreeBSD' license",
+    choices: [
+      "Apache License 2.0",
+      "BSD 3-Clause 'New' or 'Revised' license",
+      "BSD 2-Clause 'Simplified' or 'FreeBSD' license",
+    ],
   },
 ];
 
+const generateREADME = (answers) =>
+  `This is my README file ${answers.installation}`;
 //1. Table of Contents (Optional)
 
 // If your README is very long, add a table of contents to make it easy for users to find what they need.
@@ -48,7 +70,10 @@ function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-  inquirer.prompt(questions).then((input) => console.log(input));
+  inquirer.prompt(questions).then((input) => {
+    console.log(generateREADME(input));
+    // console.log(input); (run later)
+  });
 }
 
 // function call to initialize program
