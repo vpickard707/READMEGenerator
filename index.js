524484b5ae4c9e78b5c791b9cd9b30a95e2d62b5
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-21 19:18:10 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,79 +1,182 @@
+// Global
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
 
-// array of questions for user
+// Internal NPMs
+const api = require("./utils/api.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// Array of questions for user
 const questions = [
+  // GitHub Username
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username.",
+    // We need to validate that user entered at least one word
+    // https://stackoverflow.com/questions/57321266/how-to-test-inquirer-validation
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("You must enter a GitHub username.");
+      }
+      return true;
+    },
+  },
+  // GitHub Repository
+  {
+    type: "input",
+    name: "repository",
+    message: "Enter the name of your repository on GitHub.",
+    // We need to validate that user entered at least one word
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log(
+          "You must enter the name of your GitHub repository."
+        );
+      }
+      return true;
+    },
+  },
+  // Title of Project
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of your project.",
+    // We need to validate that user entered at least one word
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("You must enter the title of your project.");
+      }
+      return true;
+    },
+  },
+  // Project Description
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project.",
+    // We need to validate that user entered at least one word
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return console.log("You must enter a description for your project.");
+      }
+      return true;
+    },
+  },
+  // Project Installation
   {
     type: "input",
     name: "installation",
     message:
-      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+      "Explain how user would install (if necessary) for Installation Section.",
+    // Validation not required if question is optional
   },
-  {
-    type: "input",
-    name: "credits",
-    message:
-      "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
-  },
-
+  // Usage of project
   {
     type: "input",
     name: "usage",
     message:
-      "Provide instructions and examples for use. Include screenshots as needed.",
+      "Enter your project instructions and examples of it in use for Usage Section.",
   },
+  // Select license
   {
     type: "list",
     name: "license",
-    message:
-      "The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. The following OSI-approved licenses are popular, widely used, or have strong communities. Apache License 2.0, BSD 3-Clause 'New' or 'Revised' license, BSD 2-Clause 'Simplified' or 'FreeBSD' license",
+    message: "Choose your license for your project.",
+    // https://docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository
     choices: [
-      "Apache License 2.0",
-      "BSD 3-Clause 'New' or 'Revised' license",
-      "BSD 2-Clause 'Simplified' or 'FreeBSD' license",
+      "afl-3.0",
+      "apache-2.0",
+      "artistic-2.0",
+      "bsl-1.0",
+      "bsd-2-clause",
+      "bsd-3-clause",
+      "bsd-3-clause-clear",
+      "cc",
+      "cc0-1.0",
+      "cc-by-4.0",
+      "cc-by-sa-4.0",
+      "wtfpl",
+      "ecl-2.0",
+      "epl-1.0",
+      "epl-2.0",
+      "eupl-1.1",
+      "agpl-3.0",
+      "gpl",
+      "gpl-2.0",
+      "gpl-3.0",
+      "lgpl",
+      "lgpl-2.1",
+      "lgpl-3.0",
+      "isc",
+      "lppl-1.3c",
+      "ms-pl",
+      "mit",
+      "mpl-2.0",
+      "osl-3.0",
+      "postgresql",
+      "ofl-1.1",
+      "ncsa",
+      "unlicense",
+      "zlib",
     ],
+  },
+  // Contributing to project
+  {
+    type: "input",
+    name: "contributing",
+    message: "Explain how users can contribute to your project (if necessary).",
+    // Validation not required if question is optional
+  },
+  // Test for project
+  {
+    type: "input",
+    name: "tests",
+    message:
+      "Provide tests for project, and explain how to test (if necessary).",
+    // Validation not required if question is optional
   },
 ];
 
-const generateREADME = (answers) =>
-  `This is my README file ${answers.installation}`;
-//1. Table of Contents (Optional)
-
-// If your README is very long, add a table of contents to make it easy for users to find what they need.
-
-// - [Installation](#installation)
-// - [Usage](#usage)
-// - [Credits](#credits)
-// - [License](#license)
-/*## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-## Usage
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-## License
-
-// The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)*/
-
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your markdown file has been created.");
+  });
+}
+
+// Reference: https://www.npmjs.com/package/util.promisify
+const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
-function init() {
-  inquirer.prompt(questions).then((input) => {
-    console.log(generateREADME(input));
-    // console.log(input); (run later)
-  });
+async function init() {
+  // https://www.w3schools.com/js/js_errors.asp
+  try {
+    // Reference inquirer array with prompts
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+    // https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
+    const userResponses = await inquirer.prompt(questions);
+    console.log("Your responses: ", userResponses);
+    console.log("Your responses have been logged. Calling to GitHub...");
+
+    // Referencing API.js
+    const userInfo = await api.getUser(userResponses);
+    console.log("Your GitHub user info: ", userInfo);
+
+    // Pass inquirer data and api data to markdown
+    console.log("Generating your markdown");
+    const markdown = generateMarkdown(userResponses, userInfo);
+    console.log(markdown);
+
+    // Write markdown
+    await writeFileAsync("ExampleREADME.md", markdown);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // function call to initialize program
